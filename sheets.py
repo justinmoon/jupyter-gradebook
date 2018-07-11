e7fa727d3310f6ce0a494c39a1ac7cc026656c1d
Justin Moen <jamoen7@gmail.com>
2018-07-10 19:05:43 -0500

    Script can "pass" / "fail" to correct cell

--- sheets.py
+++ sheets.py
@@ -1,19 +1,31 @@
+from collections import namedtuple
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-
 gc = gspread.authorize(credentials)
+wks = gc.open("gradebook").sheet1
 
-# Open a worksheet from spreadsheet with one shot
-key = "https://docs.google.com/spreadsheets/d/13iWTp5TkuuZUxjLgdfk3Y_Aqy4CatIlhs54ZfrJ3xes"
-key = "gradebook"
-wks = gc.open(key).sheet1
 
-wks.update_acell('B2', "it's down there somewhere, let me take another look.")
+Grade = namedtuple('Grade', ['student_id', 'exercise_id', 'result'])
+Grid = namedtuple('Grid', ['exercises_axis', 'students_axis'])
 
-# Fetch a cell range
-cell_list = wks.range('A1:B7')
+
+def record_grade(grid, grade):
+    # TODO: error handling
+    # HACK: Sheets indexes from 1 ... this will cause bugs
+    row = grid.students_axis.index(grade.student_id) + 1
+    col = grid.exercises_axis.index(grade.exercise_id) + 1
+    wks.update_cell(row, col, grade.result)
+
+def grid():
+    # A hack. wks.find('query') was causing some weird error ...
+    exercises = wks.row_values(1)
+    students = wks.col_values(1)
+    return Grid(exercises_axis=exercises, students_axis=students)
+
+
+if __name__ == '__main__':
+    grade = Grade('justin-moen', 'lesson-2', 'pass')
+    record_grade(grid(), grade)
